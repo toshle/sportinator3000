5e054e15193ed4c33f138910b1d48d811e12f988
antonionikolov <antonionaidenovnikolov@abv.bg>
2014-04-25 22:41:44 +0300

    Add Activity

--- sportinator3000/sports/models.py
+++ sportinator3000/sports/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Activity(models.Model):
+    sport = models.ForeignKey(Sport)
+    name = models.CharField(max_length=200)
+    has_trainer = models.BooleanField()
+    price = models.FloatField()
+    duration = models.IntegerField()
+    worktime = models.CharField(max_length=50)
