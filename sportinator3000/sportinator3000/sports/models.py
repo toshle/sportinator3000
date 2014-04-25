84834f1b1a8644880f8a5db5021557c683da0f99
staticbg <staticbg@gmail.com>
2014-04-25 22:29:42 +0300

    added Sport table

--- sportinator3000/sportinator3000/sports/models.py
+++ sportinator3000/sportinator3000/sports/models.py
@@ -17,8 +17,8 @@ class Place(models.Model):
     name = models.CharField(max_length=50, unique=True)
     city = models.CharField(max_length=30, choices=CITIES)
     address = models.CharField(max_length=200)
-    photo = models.CharField(max_length=300, blank=True)
-    video_link = models.CharField(max_length=300, blank=True)
+    photo_url = models.CharField(max_length=300, blank=True)
+    video_url = models.CharField(max_length=300, blank=True)
     latitude = models.IntegerField()
     longitude = models.IntegerField()
     description = models.TextField()
@@ -26,3 +26,8 @@ class Place(models.Model):
     
     def __str__():
         return '{}, {}, {}'.format(name, city, address)
+
+
+class Sport(models.Model):
+    name = models.CharField(max_length=100)
+    photo_url = models.CharField(max_length=300, blank=True)
