2beabc847ba67476109b9b309126053ecf1907eb
Angel Tsanev <archa40@gmail.com>
2014-04-25 23:15:48 +0300

    style changes

--- sportinator3000/sports/models.py
+++ sportinator3000/sports/models.py
@@ -15,21 +15,21 @@ CITIES = (
 
 
 class Place(models.Model):
-    name = models.CharField(max_length=50, unique=True)
-    city = models.CharField(max_length=30, choices=CITIES)
-    address = models.CharField(max_length=200)
-    photo_url = models.CharField(max_length=300, blank=True)
-    video_url = models.CharField(max_length=300, blank=True)
-    latitude = models.IntegerField()
-    longitude = models.IntegerField()
+    name        = models.CharField(max_length=50, unique=True)
+    city        = models.CharField(max_length=30, choices=CITIES)
+    address     = models.CharField(max_length=200)
+    photo_url   = models.CharField(max_length=300, blank=True)
+    video_url   = models.CharField(max_length=300, blank=True)
+    latitude    = models.IntegerField()
+    longitude   = models.IntegerField()
     description = models.TextField()
-    date_added = models.DateTimeField(default=datetime.now())
+    date_added  = models.DateTimeField(default=datetime.now())
     
     def __str__():
         return '{}, {}, {}'.format(name, city, address)
 
 class Sport(models.Model):
-    name = models.CharField(max_length=100)
+    name      = models.CharField(max_length=100)
     photo_url = models.CharField(max_length=300, blank=True)
     
     def __str__():
@@ -37,13 +37,13 @@ class Sport(models.Model):
 
 
 class Activity(models.Model):
-    sport = models.ForeignKey(Sport)
-    place = models.ForeignKey(Place)
-    name = models.CharField(max_length=200)
+    sport       = models.ForeignKey(Sport)
+    place       = models.ForeignKey(Place)
+    name        = models.CharField(max_length=200)
     has_trainer = models.BooleanField()
-    price = models.FloatField()
-    duration = models.IntegerField()
-    worktime = models.CharField(max_length=50)
+    price       = models.FloatField()
+    duration    = models.IntegerField()
+    worktime    = models.CharField(max_length=50)
 
     def __str__():
         return '{}'.format(name)
