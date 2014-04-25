cd5129ef2449bcd6f0482d58d6ce7742a21e1b52
toshle <rosen_ra4ev@abv.bg>
2014-04-26 02:10:31 +0300

    Add basic templates Add basic urls

--- sportinator3000/sports/views.py
+++ sportinator3000/sports/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.template import RequestContext, loader
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+  return render(request, 'sports/home.html', {})
