0bbd37803aeddae726ed21a81fc6dc1cf31bf606
toshle <rosen_ra4ev@abv.bg>
2014-04-26 15:09:04 +0300

    Add HttpResponse

--- sportinator3000/sports/views.py
+++ sportinator3000/sports/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.template import RequestContext, loader
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
+from django.http import HttpResponse
 import math
 from sports.models import Place, PlaceActivity, Activity, Sport
 import json
