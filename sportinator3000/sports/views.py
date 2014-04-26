42ba5c9ad0f42f11a13ceabc21012e249cab7dd3
staticbg <staticbg@gmail.com>
2014-04-26 04:07:47 +0300

    added login in views

--- sportinator3000/sports/views.py
+++ sportinator3000/sports/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.template import RequestContext, loader
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+
 
 def home(request):
     return render(request, 'sports/home.html', {})
@@ -16,4 +18,17 @@ def sports_content(request):
 
 def nearby(request):
     pass
-    
+
+def user_login(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        if user.is_active:
+            login(request, user)
+            return render(request, 'sports/home.hmtl', {})
+        else:
+            return render(request, 'sports/register.html',
+                          {'disabled_account': 'Disabled account'})
+    else:
+        return render(request, 'sports/register.html', {})
