81ac63c849acbd1630c0f1bb39f585dc4e3478d7
staticbg <staticbg@gmail.com>
2014-04-26 17:06:04 +0300

    fixed the migrations

---
+++ sportinator3000/sports/migrations/0003_auto_20140426_1705.py
@@ -0,0 +1,20 @@
+# encoding: utf8
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import datetime
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('sports', '0002_placeactivity'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='place',
+            name='date_added',
+            field=models.DateTimeField(default=datetime.datetime(2014, 4, 26, 17, 5, 30, 856665)),
+        ),
+    ]
