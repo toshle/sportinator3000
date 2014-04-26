073105784e6fe7b7cf504f7e37506e4aa94962e5
staticbg <staticbg@gmail.com>
2014-04-26 05:02:42 +0300

    added nearby in views

--- sportinator3000/sports/views.py
+++ sportinator3000/sports/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from django.template import RequestContext, loader
 from django.contrib.auth import authenticate, login, logout
 import math
+from sports.models import Place
+from simplejson import *
 
 def home(request):
     return render(request, 'sports/home.html', {})
@@ -14,9 +16,6 @@ def sports(request):
 
 def sports_content(request):
     return render(request, 'sports/sports_content.html', {})
-
-def nearby(request):
-    pass
 
 def user_login(request):
     username = request.POST['username']
@@ -34,35 +33,42 @@ def user_login(request):
 
 def user_logout(request):
     logout(request)
-    return render(request, 'sports/homer.html', {})
+    return render(request, 'sports/home.html', {})
 
-def get_distance(request):
-    def distance_between_points(latitude1, longitude1, latitude2, longitude2): #returns distance in kilometers
+def nearby(request):
+    latitude1 = request.POST['latitude']
+    longitude1 = request.POST['latitude']
+    radius = request.POST['radius']
 
-    # Convert latitude and longitude to 
-    # spherical coordinates in radians.
-    degrees_to_radians = math.pi/180.0
-        
-    # phi = 90 - latitude
-    phi1 = (90.0 - latitude1)*degrees_to_radians
-    phi2 = (90.0 - latitude2)*degrees_to_radians
-        
-    # theta = longitude
-    theta1 = longitude1*degrees_to_radians
-    theta2 = longitude2*degrees_to_radians
-        
-    # Compute spherical distance from spherical coordinates.
-        
-    # For two locations in spherical coordinates 
-    # (1, theta, phi) and (1, theta, phi)
-    # cosine( arc length ) = 
-    #    sin phi sin phi' cos(theta-theta') + cos phi cos phi'
-    # distance = rho * arc length
-    
-    cos = (math.sin(phi1)*math.sin(phi2)*math.cos(theta1 - theta2) + 
-           math.cos(phi1)*math.cos(phi2))
-    arc = math.acos( cos )
+    def distance_between_points(latitude1, longitude1, latitude2, longitude2):
+        degrees_to_radians = math.pi/180.0
+        phi1 = (90.0 - latitude1)*degrees_to_radians
+        phi2 = (90.0 - latitude2)*degrees_to_radians
+        theta1 = longitude1*degrees_to_radians
+        theta2 = longitude2*degrees_to_radians
+        cos = (math.sin(phi1)*math.sin(phi2)*math.cos(theta1 - theta2) + 
+               math.cos(phi1)*math.cos(phi2))
+        arc = math.acos(cos)
 
-    # Remember to multiply arc by the radius of the earth 
-    # in your favorite set of units to get length.
-    return arc*6373
+        return arc*6373
+
+    close_places = [place for place in Place.objects.all()
+                    if self.distance_between_points(latitude1, longitude1,
+                                                    place.latitude,
+                                                    place.longitude) <= radius]
+
+    context = []
+    for place in close_places:
+        json_place = {
+            'name' : place.name
+            'latitude' : place.latitude
+            'longitude' : place.longitude
+            'description' : place.description
+            'address' : place.address
+            'city' : place.city
+            'photo_url' : place.photo_url
+            'video_url' : place.video_url
+            'date_added' : place.date_added}
+        context.append(json_place)
+
+    return render(request, 'sports/nearby.html', simplejson.dumps(context))
